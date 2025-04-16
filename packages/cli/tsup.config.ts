d3ce27159cb78992825a2aba24f633d6a5b8e300
Pol Gubau Amores <gubaupol@gmail.com>
2025-04-16 18:30:47 +0200

    refactor: (cli) ♻️ update package.json and tsup.config for module support

--- packages/cli/tsup.config.ts
+++ packages/cli/tsup.config.ts
@@ -1,18 +1,19 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-	entry: ["bin.ts"],
+	// entry: ["bin.ts"],
+	entry: ["src/index.ts"],
 	outDir: "dist",
-	format: ["esm"],
+	format: ["esm", "cjs"],
 	target: "node18",
 	splitting: false,
 	shims: false,
 	clean: true,
-	dts: false,
 	minify: false,
 	sourcemap: true,
 	bundle: true,
 	platform: "node",
+	dts: true,
 	banner: {
 		js: "#!/usr/bin/env node",
 	},
